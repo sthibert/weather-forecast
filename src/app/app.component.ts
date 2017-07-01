67b6c2593321b3bf5816ad2e52713dabac41f449
Simon Thibert <thibert.simon@gmail.com>
2017-07-01 16:46:29 +0200

    Add NProgress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { WeatherService } from './weather/weather.service';
 import { Weather } from './weather/model/weather';
 
+declare let NProgress: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,12 +14,20 @@ export class AppComponent implements OnInit {
   weather: Weather;
 
   constructor(private weatherService: WeatherService) {
+    NProgress.configure({
+      minimum: 0.2,
+      template: '<div class="bar" role="bar"><div class="peg"></div></div></div>'
+    });
   }
 
   ngOnInit(): void {
+    NProgress.start();
     this.weatherService
       .getWeather()
-      .subscribe(weather => this.weather = weather);
+      .subscribe(weather => {
+        this.weather = weather;
+        NProgress.done();
+      });
   }
 
 }
